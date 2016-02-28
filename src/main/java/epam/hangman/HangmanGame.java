83cc886bc8e66f9db4bc658bd9f1cf55f568f883
Andrei Sukhoverka <Andrei_Sukhoverka@epam.com>
2016-02-28 20:30:19 +0300

    Add level validation

--- src/main/java/epam/hangman/HangmanGame.java
+++ src/main/java/epam/hangman/HangmanGame.java
@@ -5,6 +5,9 @@ public class HangmanGame {
     private int level;
     
     public void setLevel(int level) {
+        if(level < 3 || level > 7){
+            throw new IllegalArgumentException("Level must be between 3 and 7");
+        }
         this.level = level;        
     }
     
--- src/test/java/epam/hangman/HangmanTest.java
+++ src/test/java/epam/hangman/HangmanTest.java
@@ -13,5 +13,17 @@ public class HangmanTest {
         assertEquals("Level must be 6", 6, hangman.getLevel());
     }
     
+    @Test(expected = IllegalArgumentException.class)
+    public void userShouldNotBeAbleToChooseLevelLessThanThree() {
+        HangmanGame hangman = new HangmanGame();
+        hangman.setLevel(2);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void userShouldNotBeAbleToChooseLevelHigherThanSeven() {
+        HangmanGame hangman = new HangmanGame();
+        hangman.setLevel(8);
+    }
+    
 
 }
