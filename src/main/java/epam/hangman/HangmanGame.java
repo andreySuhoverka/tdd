1d27daad30a778c9b5dff0c7ec5de02037260e19
Andrei Sukhoverka <Andrei_Sukhoverka@epam.com>
2016-02-28 20:24:49 +0300

    Add level support

---
+++ src/main/java/epam/hangman/HangmanGame.java
@@ -0,0 +1,15 @@
+package epam.hangman;
+
+public class HangmanGame {
+    
+    private int level;
+    
+    public void setLevel(int level) {
+        this.level = level;        
+    }
+    
+    public int getLevel() {
+        return level;
+    }
+    
+}
--- src/test/java/epam/hangman/HangmanTest.java
+++ src/test/java/epam/hangman/HangmanTest.java
@@ -1,6 +1,17 @@
 package epam.hangman;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class HangmanTest {
+    
+    @Test
+    public void userShouldBeAbleToChooseTheLevel() {
+        HangmanGame hangman = new HangmanGame();
+        hangman.setLevel(6);
+        assertEquals("Level must be 6", 6, hangman.getLevel());
+    }
     
 
 }
