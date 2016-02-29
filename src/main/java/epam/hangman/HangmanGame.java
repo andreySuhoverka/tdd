a3d6b790eea9400cd853e09ea9c55ffe1df2d27d
asukhoverka <suhoverk.andrey@gmail.com>
2016-02-29 20:48:25 +0300

    Complete the game

--- src/main/java/epam/hangman/HangmanGame.java
+++ src/main/java/epam/hangman/HangmanGame.java
@@ -1,9 +1,16 @@
 package epam.hangman;
 
+import java.io.Console;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class HangmanGame {
+
+    public static void main(String[] args) {
+        new HangmanGame().startGame();
+    }
 
     HangmanGame() {
         setDefaultLevel();
@@ -23,12 +30,23 @@ public class HangmanGame {
     
     private int level;
     private int madeSteps;
+    private String actualWord;
     
     public void setLevel(int level) {
         if(level < 3 || level > 7){
             throw new IllegalArgumentException("Level must be between 3 and 7");
         }
-        this.level = level;        
+        this.level = level;
+        actualWord = createInitialWord(level);
+
+    }
+
+    private String createInitialWord(int level) {
+        String result = "";
+        for(int k = 0; k < level; k++) {
+            result += "_";
+        }
+        return result;
     }
     
     public int getLevel() {
@@ -36,7 +54,7 @@ public class HangmanGame {
     }
     
     private void setDefaultLevel(){
-        level = 3;
+        setLevel(3);
     }
 
     public String obtainWordAccordingToTheLevel(int level) {
@@ -57,6 +75,67 @@ public class HangmanGame {
 
     public boolean userAlive() {
         return madeSteps < level + 1;
+    }
+
+    public void startGame(){
+        Console console = System.console();
+        if(console == null){
+            throw new RuntimeException("There is no console available!");
+        }
+        System.out.print("Choose word length: ");
+        String level = console.readLine();
+        setLevel(Integer.valueOf(level));
+        while(needNextStep()){
+            System.out.print("Guess letter: ");
+            String letter = console.readLine();
+            boolean guessed = this.guessOneLetter(letter);
+            handleActualWord(letter, guessed);
+            showCurrentGameStatus();
+            System.out.println();
+        }
+        showGameResult();
+    }
+
+    private void showGameResult() {
+        if(!actualWord.contains("_")){
+            System.out.println("Winner, you are alive!");
+        } else {
+            System.out.println("unfortunately you died :(");
+        }
+    }
+
+    private void showCurrentGameStatus() {
+        System.console().printf("Current word is: " + actualWord);
+    }
+
+    private void handleActualWord(String letter, boolean guessed) {
+        if(guessed) {
+            actualWord = updateActualWordWithLetter(letter);
+        }
+
+    }
+
+    private String updateActualWordWithLetter(String letter) {
+        String word = obtainWordAccordingToTheLevel(getLevel());
+        List<Integer> guessedPositions = new ArrayList<Integer>();
+        for (int k = 0; k < word.length(); k++) {
+            if(word.substring(k, k + 1).equals(letter)) {
+                guessedPositions.add(k);
+            }
+        }
+        String updatedWord = "";
+        for (int k = 0; k < word.length(); k++) {
+            if(guessedPositions.contains(k)) {
+                updatedWord += letter;
+            }else {
+                updatedWord += actualWord.charAt(k);
+            }
+        }
+        return updatedWord;
+    }
+
+    private boolean needNextStep() {
+        return userAlive() && actualWord.contains("_");
     }
 
 }
